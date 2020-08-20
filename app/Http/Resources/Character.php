1c5865f221a09b8da4f8adc2116625dc9bb6d672
Arnaldo Souza <arnaldoeloi2@gmail.com>
2020-08-20 16:26:26 -0300

    Added random_memorable_quote to CharacterResource.

--- app/Http/Resources/Character.php
+++ app/Http/Resources/Character.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-use App\Http\Resources\QuoteCollection;
+use App\Http\Resources\Quote as QuoteResource;
 
 class Character extends JsonResource
 {
@@ -17,18 +17,18 @@ class Character extends JsonResource
     public function toArray($request)
     {
         return [
-            'name'              => $this->name,
-            'gang'              => $this->gang ? $this->gang->name : null,
-            'gender'            => $this->gender,
-            'age'               => $this->age,
-            'status'            => $this->status,
-            'cause_of_death'    => $this->cause_of_death,
-            'date_of_birth'     => $this->date_of_birth,
-            'nationality'       => $this->nationality,
-            'voiced_by'         => $this->voiced_by,
-            'image'             => $this->image,
-            'artwork'           => $this->artwork,
-            'quotes'            => new QuoteCollection($this->quotes)
+            'name'                     => $this->name,
+            'gang'                     => $this->gang ? $this->gang->name : null,
+            'gender'                   => $this->gender,
+            'age'                      => $this->age,
+            'status'                   => $this->status,
+            'cause_of_death'           => $this->cause_of_death,
+            'date_of_birth'            => $this->date_of_birth,
+            'nationality'              => $this->nationality,
+            'voiced_by'                => $this->voiced_by,
+            'image'                    => $this->image,
+            'artwork'                  => $this->artwork,
+            'random_memorable_quote'   => $this->quotes->count() ? $this->quotes->random()->quote : null, 
         ];
     }
 }
