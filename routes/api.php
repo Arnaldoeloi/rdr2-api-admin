403e6ccdbbb97d6d506f4fb5ebc4efdbd47d04f8
Arnaldo Souza <arnaldoeloi2@gmail.com>
2021-09-09 21:10:28 -0300

    Added command on post-update-cmd.

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('characters', 'CharacterController', ['except' => ['store, update, delete']]);
+Route::resource('characters', 'CharacterController');
